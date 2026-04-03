eb2f5330a161798bf40158fa4c04091a1e2f811a
Kallel2026 <antoniokallel15@gmail.com>
2026-04-03 22:51:58 +0000

    hg

---
+++ algoritmos C/aula 1/3ex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    const double PI = 3.14159;
+    double a, b, c, result;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    result = (a * c) / 2;
+    printf("TRIANGULO: %0.3lf\n", result);
+
+    result = PI * c * c;
+    printf("CIRCULO: %0.3lf\n", result);
+
+    result = ((a * b) * c) / 2;
+    printf("TRAPEZIO: %0.3lf\n", result);
+
+    result = b * b;
+    printf("QUADRADO: %0.3lf\n", result);
+
+    result = a * b;
+    printf("RETANGULO: %0.3lf\n", result);
+    return 0,
+}
